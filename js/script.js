5240f179f5abcf4fd9fd927603f6e51e6e30d9e9
Binotz <binotto.riccardo@gmail.com>
2022-07-04 15:13:37 +0200

    first commit

---
+++ js/script.js
@@ -0,0 +1,20 @@
+var app = new Vue(
+    {
+        el:'.app',
+        data:{
+            emails: []
+        },
+        methods:{
+        },
+        mounted(){
+            for(let i = 0; i < 10; i++){
+                axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+                .then((resp)=>{
+                    let currentEmail = resp.data.response;
+                    this.emails.push(currentEmail);
+                    console.log(this.emails.length);
+                });
+            }
+        }
+    }
+);
